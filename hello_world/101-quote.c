a00bec895fac858b25b32b57933c0d26d3e112f1
mackun-ui <m.ackun@alustudent.com>
2026-09-04 12:34:17 -0700

    question 8

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(STDERR_FILENO, quote, 61);
+	write(STDERR_FILENO, quote, 59);
 
 	return (1);
 }
