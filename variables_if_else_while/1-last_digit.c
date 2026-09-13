22f47fdd9c8b6a62bd52711d4af5aa9796c04cf4
mackun-ui <m.ackun@alustudent.com>
2026-09-13 10:40:16 -0700

    question 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a random number and prints its lasts digit
+ * with a classification
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n, last;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	last = n % 10;
+
+	printf("Last digit of %d is %d and is ", n, last);
+
+	if (last > 5)
+		printf("greater than 5\n");
+	else if (last == 0)
+		printf("0\n");
+	else 
+		printf("less than 6 and not 0\n");
+
+	return (0);
+}
